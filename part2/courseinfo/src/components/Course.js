9af829382968ec2bf548992067a45ed58cfce91b
idcristi <cristi.duluta@gmail.com>
2020-04-08 20:33:10 +0300

    Add part 2 exercises 2.4 step9 - 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -3,14 +3,14 @@ import Header from './Header';
 import Content from './Content';
 import Total from './Total';
 
-const Course = ({course}) => {
-  return (
-    <div>
+const Course = ({courses}) => {
+  return courses.map((course) => (
+    <div key={course.id}>
       <Header course={course} />
       <Content course={course} />
-      <Total course={course.parts} />
+      <Total course={course} />
     </div>
-  );
+  ));
 };
 
 export default Course;
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Total = ({course}) => {
-  const total = course
+  const total = course.parts
     .map((number) => number.exercises)
     .reduce((acc, value) => acc + value, 0);
   return <p style={{fontWeight: 'bold'}}>total of {total} exercises</p>;
