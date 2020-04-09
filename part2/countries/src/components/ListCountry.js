81eba706c0159c96dce3f2e002e94046ce4d2b02
idcristi <cristi.duluta@gmail.com>
2020-04-09 20:36:16 +0300

    Add part 2 exercise 2.13 step 2

--- part2/countries/src/components/ListCountry.js
+++ part2/countries/src/components/ListCountry.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Button from './Button';
 
-const ListCountry = ({countries}) => countries.map((country) => <div key={country.name}>{country.name}</div>);
-
+const ListCountry = ({countries, updateCountryName}) =>
+  countries.map((country) => (
+    <div key={country.name}>
+      {country.name}
+      <Button onClick={() => updateCountryName(country.name)} text="show" />
+    </div>
+  ));
 export default ListCountry;
